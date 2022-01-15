3b2a29d0b0b855bc546f67b3a304dc511bb88db1
Wheels McFantini <WheelsMcFantini@gmail.com>
2022-01-15 14:58:34 -0500

    proper exit

--- index.js
+++ index.js
@@ -121,13 +121,8 @@ async function playGame(mine) {
 
 
 async function gameRunner() {
-<<<<<<< HEAD
-  if (ACTIVE == 'False'){exit()}
+  if (ACTIVE == 'False'){process.exit(0)}
   logger.info(`[Game-runner] Retrieving lastest game ID for ${ADDRESS}`)
-=======
-  if (ACTIVE == 'False'){process.exit()}
-  console.log(`[Game-runner] Retrieving lastest game ID for ${ADDRESS}`)
->>>>>>> main
   game_info = await retrieveLatestGameInfo(ADDRESS)
   logger.info(game_info)
   if (game_info['game_id'] == 'NO_GAME') {
