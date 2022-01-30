8df0a6bc989607cce46a2c138c251c52259d4d8a
Wheels McFantini <WheelsMcFantini@gmail.com>
2022-01-30 15:25:51 -0500

    fixed crabada-tx to match known working

--- crabada-tx.js
+++ crabada-tx.js
@@ -17,22 +17,11 @@ const logger = createLogger({
     format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss'
     }),
-    format((info, opts) => {
-        let level = info.level.toUpperCase();
-          if(level === 'VERBOSE') {
-            level = 'DEBUG';
-          }
-  
-          info['severity'] = level;
-          delete info.level;
-          return info;
-      })(),
     format.errors({ stack: true }),
     format.splat(),
     format.json()),
     transports: [
-    new transports.Console()
-    //loggingWinston
+    new transports.Console(),
     //new transports.File({ filename: 'combined.log' })
   ]
 });
